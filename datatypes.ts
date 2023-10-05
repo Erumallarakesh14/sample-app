8a14c4205641a022222c46424a92a7a8bb22b8ab
Rakesh Erumalla <rakesh.erumalla@zelis.com>
2023-10-05 06:34:22 -0700

    typescript

---
+++ datatypes.ts
@@ -0,0 +1,18 @@
+let lname : string;
+lname="rakesh";
+let n=lname.toUpperCase();
+//n=20;
+let isvalid:boolean=false ;
+let arr:Array<number>;
+arr=[1,2,3,4,5];
+let sum=arr.reduce((acc,num)=>acc+num);
+let arr1 :(string | number)[]=[1,"rakesh",2,3]
+//let result=arr.filter((num)=>num>2);
+//let results=arr.find((num)=>num==2);
+let p=arr1.pop();
+arr1.push(5);
+console.log(arr1);
+console.log(sum);
+
+console.log(n);
+console.log(isvalid);
